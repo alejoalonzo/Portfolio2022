7f32ce3a87191e8d3f7250cb8f19f76d661e0278
alejandro <alejandro.12w@gmail.com>
2022-02-26 16:23:57 +0100

    porfolio init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { AboutComponent } from './components/about/about.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    CreateComponent,
+    AboutComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { AboutComponent } from './components/about/about.component';
+import { ErrorComponent } from './components/error/error.component';
+//import { homedir } from "os";
+
+
+const appRoutes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'about-me', component:AboutComponent},
+    {path: 'contact', component:ContactComponent},
+    {path: 'projects', component:ProjectsComponent},
+    {path: 'create', component:CreateComponent},
+    {path: '**', component:ErrorComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any>= RouterModule.forRoot(appRoutes);
