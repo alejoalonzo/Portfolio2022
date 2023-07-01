569aa41281a43aa33ef24c3a937adfde5c37f9f9
alejandro <alejandro.12w@gmail.com>
2023-06-30 21:47:21 -0700

    updating angular v13

---
+++ src/app/components/text-three/text-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-three',
+  templateUrl: './text-three.component.html',
+  styleUrls: ['./text-three.component.scss']
+})
+export class TextThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
