e120e470dd13c1c2e0b007c2390f75441814d54a
alejandro <alejandro.12w@gmail.com>
2023-07-08 21:14:26 -0700

    before upgrading Angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  recaptcha: {
+    siteKey: '6LdKnAknAAAAAG9MJOy1zTIW9I0m7WCBtkBzN2fg',
+},
 };
 
 /*
